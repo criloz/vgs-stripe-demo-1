71c84599a321b32f40ff8bcfc6ddf39bfbfe27e3
Cristian Lozano <criloz@nebtex.com>
2019-08-12 08:48:30 -0500

    initial

---
+++ src/public/js/credit-card-example.js
@@ -0,0 +1,62 @@
+var f = VGSCollect.create('tntvisauu0i', function (state) {
+});
+
+var field = f.field('#cc-name .form-control', {
+    type: 'text',
+    name: 'cardName',
+    placeholder: 'Joe Business',
+    validations: ['required'],
+});
+
+f.field('#cc-number .form-control', {
+    type: 'card-number',
+    name: 'cardNumber',
+    successColor: '#4F8A10',
+    errorColor: '#D8000C',
+    placeholder: '4111 1111 1111 1111',
+    validations: ['required', 'validCardNumber'],
+});
+
+f.field('#cc-cvc .form-control', {
+    type: 'card-security-code',
+    name: 'cardCvc',
+    placeholder: '344',
+    validations: ['required', 'validCardSecurityCode'],
+});
+
+f.field('#cc-expiration-date .form-control', {
+    type: 'card-expiration-date',
+    name: 'cardExpirationDate',
+    placeholder: '01 / 2022',
+    validations: ['required', 'validCardExpirationDate']
+});
+
+var elements = document.querySelectorAll('label');
+for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', function (t) {
+        field.focus();
+    });
+}
+
+document.getElementById('cc-form')
+    .addEventListener('submit', function (e) {
+        let targetForm = e.target;
+        e.preventDefault();
+        $('#purchase-btn').prepend('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>');
+        //submit and send the amount of the transaction
+        f.submit('/post', {data: {amount: urlParams['amount']}}, function (status, data) {
+            if (data && data.kind) {
+                if (data.kind === "transaction_succeeded_without_3ds") {
+                    //close modal
+                    parent.postMessage({kind: "close-modal", data: {kind: "transaction_succeeded_without_3ds"}}, "*")
+                } else if (data.kind === "action_redirect") {
+                    //close modal
+                    window.location.replace(data.redirect_url);
+                }
+            }
+
+            cleanErrorMessages(targetForm);
+        }, function (errors) {
+            highlightErrors(targetForm, errors);
+        });
+    }, false);
